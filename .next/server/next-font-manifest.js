a511e38cd0b69ea5bafa5dc0d1c4a29c51bed8df
Nataliia Bohdan <natalkaa755@gmail.com>
2025-03-17 13:59:02 -0600

    Initial commit

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/nataliia/Desktop/the-wild-oasis-website/app/layout\":[\"static/media/a3a474206a856479-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/nataliia/Desktop/the-wild-oasis-website/app/layout":["static/media/a3a474206a856479-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
